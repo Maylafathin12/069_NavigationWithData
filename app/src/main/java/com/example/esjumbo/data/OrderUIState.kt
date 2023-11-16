58c6eee597e0320b5fd46f1dbf2409d34797d9db
TA1069 <maylafaat@gmail.com>
2023-11-16 08:55:45 +0700

    menambahkan val di bagian OrderUIState

---
+++ app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
@@ -0,0 +1,11 @@
+package com.example.esjumbo.data
+
+
+    data class OrderUIState(
+        val jumlah: Int = 0,
+        val rasa: String = "",
+        val harga: String ="",
+        val nama: String ="",
+        val alamat: String ="",
+        val telepon: String =""
+    )
--- app/src/main/java/com/example/esjumbo/data/OrderuiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.esjumbo.data
-
-
-    data class OrderuiState(
-        val jumlah: Int =0,
-        val rasa: String = "",
-        val harga: String = "",
-    )
